53ae73048ee712e517371adbbe56d117968632e1
Michael Ordaz Mrtz <tu_nuevo_correo@example.com>
2025-04-02 17:31:23 -0600

    componente search

---
+++ pages/search.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const searchQuery = ref('')
+const hasError = ref(false)
+const isDisabled = ref(false)
+</script>
+<template>
+  <h1>Buscador</h1>
+  <div class="w-3/12 p-3">
+    <UikitSearch v-model="searchQuery" :disabled="isDisabled" placeholder="Search" />
+
+    <br>
+
+    <UikitSearch v-model="searchQuery" :disabled="true" placeholder="Search" />
+
+    <br>
+
+    <UikitSearch v-model="searchQuery" error="text error" placeholder="Search" />
+
+    <br>
+    <span>{{ searchQuery }}</span>
+  </div>
+</template>
