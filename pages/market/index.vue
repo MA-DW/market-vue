695eef1a9fa0e885ccc445e77598d4bf7baf5f3d
Michael Ordaz Mrtz <tu_nuevo_correo@example.com>
2025-04-11 11:07:53 -0600

    wip home page

--- pages/market/index.vue
+++ pages/market/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+import { Doughnut, Pie } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, ArcElement)
 
 const filters = reactive({
   market: '',
@@ -15,12 +15,36 @@ const filters = reactive({
 
 
 const chartData = ref({
-  labels: [ 'January', 'February', 'March' ],
-  datasets: [ { data: [40, 20, 12] } ]
+  labels: ['Rojo', 'Azul', 'Amarillo'],
+  datasets: [
+    {
+      label: 'Distribución de colores',
+      data: [40, 30, 30],
+      backgroundColor: ['navy', '#60a5fa', 'blue'],
+      borderColor: ['#ffffff', '#ffffff', '#ffffff'],
+      borderWidth: 2,
+    }
+  ]
+})
+
+const chartDataPie = ref({
+  labels: ['Producto A', 'Producto B', 'Producto C'],
+  datasets: [
+    {
+      label: '2023',
+      data: [40, 30, 30],
+      backgroundColor: ['navy', '#60a5fa', 'blue'],
+    },
+    {
+      label: '2024',
+      data: [20, 50, 30],
+      backgroundColor: ['#fb923c', '#34d399', '#a78bfa'],
+    }
+  ]
 })
 
 const chartOptions = ref({
-  responsive: true
+  responsive: true,
 })
 </script>
 <template>
@@ -131,33 +155,180 @@ const chartOptions = ref({
 
     </div>
 
-    <div class="w-full h-80 bg-gray-500 mt-3">
-
+    <div class="w-full mt-4">
+      <img src="/assets/images/map.png" alt="fake gmap" class="w-full">
     </div>
 
-    <div class="mt-3">
-      <h1>Availability Q2 2024</h1>
+    <div class="my-5">
+      <span class="font-bold uppercase text-[32px]">INDUSTRIAL Availability | Q2 2024</span>
     </div>
 
-    <div class="flex gap-4 w-full">
-      <div class="bg-white flex-1 h-28">card</div>
-      <div class="bg-white flex-1 h-28">card</div>
-      <div class="bg-white flex-1 h-28">card</div>
-      <div class="bg-white flex-1 h-28">card</div>
-    </div>
-
-
-    <div class="flex gap-4 w-full mt-3">
-      <div class="bg-white flex-1 h-28">card</div>
-      <div class="bg-white flex-1 h-28">
-        <Bar
-          id="my-chart-id"
-          :options="chartOptions"
-          :data="chartData"
-        />
-
+    <div class="flex flex-wrap">
+      <div class="w-1/4 md:w-1/2 lg:w-1/4 pr-2">
+        <UikitCardNumber title="Card title/hame/header" text="000,000,000" value="000"></UikitCardNumber>
       </div>
-      <div class="bg-white flex-1 h-28">card</div>
+      <div class="w-1/4 md:w-1/2 lg:w-1/4 px-2">
+        <UikitCardNumber title="Card title/hame/header" text="000,000,000" value="000"></UikitCardNumber>
+      </div>
+      <div class="w-1/4 md:w-1/2 lg:w-1/4 px-2">
+        <UikitCardNumber title="Card title/hame/header" text="000,000,000" value="000"></UikitCardNumber>
+      </div>
+      <div class="w-1/4 md:w-1/2 lg:w-1/4 pl-2">
+        <UikitCardNumber title="Card title/hame/header" text="000,000,000" value="000"></UikitCardNumber>
+      </div>
+    </div>
+
+    <div class="flex flex-wrap w-full mt-4">
+      <div class="w-full md:w-1/2 lg:w-1/4 pr-2">
+        <div class="h-full rounded-lg bg-white shadow-[0_10px_30px_0_rgb(0_0_0_/_.05)] p-4">
+          <div class="font-calibri font-bold text-[20px]">Building by location</div>
+
+          <div class="mt-4 overflow-auto max-h-96">
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between text-sm font-medium text-gray-600 mb-1">
+                <span>Aguascalientes</span>
+                <span class="font-semibold text-gray-800">27.5%</span>
+              </div>
+              <div class="w-full bg-gray-200 rounded-full h-2.5">
+                <div class="bg-blue-600 h-2.5 rounded-full" style="width: 27.5%;"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-2/4 px-2">
+        <div class="h-full rounded-lg bg-white shadow-[0_10px_30px_0_rgb(0_0_0_/_.05)] p-4">
+          <div class="flex">
+            <div class="w-1/2">
+              <Pie
+                id="my-chart-id"
+                :options="chartOptions"
+                :data="chartDataPie"
+              />
+            </div>
+            <div class="w-1/2">
+              <div>
+                <div class="text-center font-calibri font-bold text-[20px]">Pecentage by Class</div>
+                <div class="flex flex-col text-center mt-10">
+                  <div class="font-bold text-[45px]">59.14%</div>
+                  <div class="text-gray-600"><div class="w-3 h-3 rounded-full mr-2 bg-primary inline-block"></div>Class A Building</div>
+                </div>
+                <div class="flex flex-col text-center mt-10">
+                  <div class="font-bold text-[45px]">45.06%</div>
+                  <div class="text-gray-600"><div class="w-3 h-3 rounded-full mr-2 bg-secondary inline-block"></div>Class A Building</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full md:w-1/2 lg:w-1/4 pl-2">
+        <div class="h-full rounded-lg bg-white shadow-[0_10px_30px_0_rgb(0_0_0_/_.05)] p-4">
+          <Doughnut
+            id="my-chart-id"
+            :options="chartOptions"
+            :data="chartData"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="my-5">
+      <span class="font-bold uppercase text-[32px]">INDUSTRIAL Listing</span>
+    </div>
+
+    <div class="shadow-[0_10px_30px_0_rgb(0_0_0_/_.05)] p-4 bg-white">
+      <div class="flex">
+        <div class="flex gap-4 flex-1">
+          Entries per page: 
+          <select name="per_pages">
+            <option>100</option>
+            <option>50</option>
+            <option>25</option>
+            <option>10</option>
+          </select>
+          Last update: 15/12/2024
+        </div>
+        <div class="flex gap-2">
+          <div class="bg-primary-100 cursor-pointer rounded p-1"><UikitIcon name="gallery" /></div>
+          <div class="bg-primary-100 cursor-pointer rounded p-1"><UikitIcon name="rows" /></div>
+          <div class="bg-primary-100 cursor-pointer rounded p-1"><UikitIcon name="compare" /></div>
+          <div class="bg-primary-100 cursor-pointer rounded p-1"><UikitIcon name="download" /></div>
+          <div class="bg-primary-100 cursor-pointer rounded p-1"><UikitIcon name="favorite" /></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
